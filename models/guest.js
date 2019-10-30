2c1106838e5b80a097134ec2f2347536a1dce918
dmarinov8 <demarinov@gmail.com>
2019-10-30 15:36:54 +0200

    initial commit

---
+++ models/guest.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+
+const guestSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    phone: {
+        type: String,
+        minlength: 5,
+        maxlength: 50
+    },
+    country: {
+        type: String,
+        minlength: 5,
+        maxlength: 50
+    }
+});
+
+
+const Guest = mongoose.model('Guest', guestSchema);
+
+function validateGuest(guest) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.email().min(5).max(50).required(),
+        phone: Joi.string().min(5).max(50),
+        country: Joi.string().min(5).max(50)
+    };
+
+    return Joi.validate(guest, schema);
+
+}
+
+
+exports.guestSchema = guestSchema;
+exports.Guest = Guest;
+exports.validateGuest = validateGuest;
+
